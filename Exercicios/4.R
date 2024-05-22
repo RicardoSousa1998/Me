5d477424042887504ca63804e85206eba631bbe0
Ricardo Sousa <60556884+RicardoSousa1998@users.noreply.github.com>
2024-05-22 02:20:04 +0100

    Update 4.R

--- Exercicios/4.R
+++ Exercicios/4.R
@@ -341,6 +341,151 @@ catalizador2 <- c(66.4, 71.7, 70.3, 69.3, 64.8, 69.6, 68.6, 69.4, 65.3, 68.8)
 #dados obtidos pelos dois catalisadores. Em media, os dados obtidos pelos
 #dois catalisadores podem ser considerados iguais?
 
-BSDA::z.test(x=catalizador1,sigma.x = sd(catalizador1),y=catalizador2,sigma.y = sd(catalizador2),conf.level = 0.95 )
-#]-5.7411764,-0.6588236[ com 95% conf  os dois catalizadoes nao podem ser  considerados iguais 
+BSDA::z.test(x=catalizador1,sigma.x = 3,y=catalizador2,sigma.y = 3,conf.level = 0.95 )
+#]−5.8296, −0.5704[ com 95% conf  os dois catalizadoes nao podem ser  considerados iguais 
 
+
+
+##### 4.7
+
+#Pretende-se investigar o nıvel de remuneracao salarial dos
+#homens e mulheres de certa categoria profissional. De duas amostras obtidas entre
+#dois grupos, destacam-se os seguintes resultados (em unidades monet´arias):
+
+#Amostra de 250 homens: x- =  33.8  s^2 = 5.7
+#Amostra de 150 mulheres: x- = 31   s^2 = 10.3
+
+#Construa um intervalo de confianca a 99% para as diferencas salariais medias
+#entre os dois generos e conclua sobre a possıvel existencia de discriminacao de
+#genero na atribuicao de remuneracoes.
+
+
+
+#População
+  #X1 remuneracao salarial do homens 
+  #X2 remuneracao salarial das mulheres
+#Amostra 
+  #n1 = 250,x-1 = 33.8 ,s^2 =5.7
+  #n2 = 150,x- = 31, s^2 = 10.3
+  
+
+#Conf = 99 = 0.99
+#alpha = 1-0.99 = 0.01
+
+# ] (x̅1 - x̅2) |-+| z_(1 - (α/2)) * sqrt((s1^2 / n1) + (s2^2 / n2)) [
+# ] (33.8 - 31) |-+| z_(1 - (0.01/2)) * sqrt((5.7 / 250) + (10.3 / 150)) [
+# ] 2.8 |-+| 2.575829 * 0.3024346 [
+# ]2.02098 ,3.57902 [
+
+
+
+##### 4.8
+
+#Deseja-se saber se um programa de reabilitacao apos enfarte
+#de miocardio diminui a frequencia cardıaca de esforco. Para tal, 10 doentes com
+#enfarte do miocardio foram submetidos a uma prova de esforco antes e depois do
+#programa. Os resultados, expressos em batimentos por minuto, estao na tabela
+#seguinte:
+
+antes <- c(147, 122 ,127 ,141, 150, 132, 157, 147, 157 ,155)
+depois <- c(132, 117, 142, 124 ,116, 130 ,122, 118 ,135, 117)
+
+#Suponha que a variavel em estudo segue uma distribuicao normal.
+
+#1  As amostra sao independentes ou emparelhadas?
+#sao emparelhadas pq cada doente foi submetido a duas medições uma antes e uma depois 
+
+
+#2  Recorrendo a um intervalo de confianca a 95% para a diferenca media, indique se acha que o programa de reabilitacao foi eficaz.
+ t.test(antes, depois, paired = TRUE, conf.level = 0.95)
+
+ 
+ 
+#### 4.9
+ #Foi estudado o grau de satisfacao (medido por questionario)
+ #de varios utentes de uma clınica dentaria antes e depois de lhes ser aplicada uma
+ #nova protese total removıvel. Os resultados, expressos em grau de satisfacao, foram
+ #os apresentados na tabela seguinte:
+ 
+ antes <- c(4 ,10, 8 ,13, 7, 3, 15, 7)
+ depois <-c(4 ,16, 11, 17, 17, 4, 18, 11)
+#1  As amostra sao independentes ou emparelhadas?
+#Sao emparelhads
+ 
+#2 Recorrendo a um intervalo de confianca a 99% para a diferenca media, 
+#indique se a aplicacao da nova protese influenciou o grau de satisfacao dos
+#utentes.
+ t.test(depois, antes, paired = TRUE, conf.level = 0.99)
+ #Com 99% de confianca, a aplicacao da nova protese influenciou o grau de satisfacao dos utentes
+ 
+ 
+ ####4.10
+ 
+ #Para comparar a eficiencia de dois metodos de ensino, uma
+ #turma de 24 alunos foi dividida aleatoriamente em dois grupos. Cada grupo ´e
+ #ensinado de acordo com um metodo diferente. Os resultados no fim do semestre
+ #sao os seguintes (numa escala de 0 a 100):
+ 
+# 1.o Grupo: n1 = 13 x̅1 = 74.5 s1^2 = 82.6
+# 2.o Grupo: n2 = 11 x̅2 = 71.8  sum[1,11](x2- x̅2) = 1126 
+ #sum[inicial,final] 
+ 
+ #Supondo que as populacoes sao normais (com variancias iguais), obteve-se o
+ #seguinte intervalo de confianca para a diferenca entre os valores esperados das duas
+ #populacoes:
+#   ] − 5.635, 11.035[.
+
+ #1. Indique qual o grau de confian¸ca utilizado no calculo deste intervalo.
+
+  
+  #s2^2 =  sum[1,11](x2- x̅2)/n2-1 = 1126/10 = 112.6  
+
+ 
+ #] (x̅1 - x̅2) |-+| t_(1 - (α/2); n1 + (n2 - 2)) * sqrt(((1 / n1) + (1 / n2)) * ((((n1 - 1) * s1^2) + ((n2 - 1) * s2^2)) / (n1 + (n2 - 2)))) [
+ #] (74.5 - 71.8) -+ t_(1 - ((1-b)/2); 13 + (11 - 2)) * sqrt(((1 / 13) + (1 / 11)) * ((((13) * 82.6) + ((11 - 1) * 112.6)) / (13 + (11 - 2)))) [
+
+ 
+ 
+ (74.5 - 71.8) + q(1 - (alpha/2), 13 + (11 - 2)) * sqrt(((1 / 13) + (1 / 11)) * ((((13) * 82.6) + ((11 - 1) * 112.6)) / (13 + (11 - 2)))) 
+ 
+ 
+ alpha<- 1
+ c <- 11.035
+ 
+ while (TRUE)
+ {
+   value <-   (74.5 - 71.8) + qt(1 - (alpha/2), 13 + (11 - 2)) * sqrt(((1 / 13) + (1 / 11)) * ((((13) * 82.6) + ((11 - 1) * 112.6)) / (13 + (11 - 2)))) 
+   
+   if (value  >= c ) {
+     break
+   }
+   if (alpha <= 0.01){
+     break
+   }
+   
+   alpha <- alpha - 0.01
+ }  
+ 
+ #como alpha é 0.05 o conf é 0.95 ou seja 95%
+ 
+ #2 Com base num intervalo de confianca a 90% acha que, em media, os metodos
+ #de ensino podem ser considerados iguais?
+   
+ # [ = (74.5 - 71.8) + qt(1 - (0.10/2), 13 + (11 - 2)) * sqrt(((1 / 13) + (1 / 11)) * ((((13) * 82.6) + ((11 - 1) * 112.6)) / (13 + (11 - 2))))
+ # ] = (74.5 - 71.8) - qt(1 - (0.10/2), 13 + (11 - 2)) * sqrt(((1 / 13) + (1 / 11)) * ((((13) * 82.6) + ((11 - 1) * 112.6)) / (13 + (11 - 2))))
+ 
+ #]-4.334361 ,9.734361[ 
+ # o intervalo de confiança inclui zero, Portanto, em media, os metodos de ensino podem ser considerados iguais com base no intervalo de confiança a 90%(ppt4 slide 76)
+
+ 
+ #3 Com base num intervalo de confian¸ca a 90% verifique se a suposicao das
+ #variancias serem iguais e valida.
+ 
+ 
+ # I.C.: ] ((1 / f_(1 - (α/2); n1 - 1; n2 - 1)) * (s1^2 / s2^2)) , 
+ # I.C.:   ((1 / f_(α/2; n1 - 1; n2 - 1)) * (s1^2 / s2^2)) [ 
+ 
+ # ] ((1 / qf(1 - (0.10/2), 12, 10)) * (82.6 / 112.6)) , ((1 / qf(0.10/2, 12, 10)) * (82.6/ 112.6)) [
+ 
+ #] 0.2518284,2.019802[ 
+ #Com 90% de confianca a suposicao das variancias serem iguais e valida pq 1 exite dentro do ic (ppt4  slide 80)
