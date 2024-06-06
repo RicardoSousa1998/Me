a2a32f7ad33ae1830f2be6c3cd637750d2595769
Ricardo Sousa <60556884+RicardoSousa1998@users.noreply.github.com>
2024-06-06 15:51:29 +0100

    RC

--- Exercicios/Capitula 5 Em aula.R
+++ Exercicios/Capitula 5 Em aula.R
@@ -31,3 +31,113 @@ BSDA::z.test(
 )
 
 
+#Exemplo2.1
+
+
+#pop
+  #'1% defeito = p = 0.01
+  #'x~binorm
+#amostra
+  #'n = 1000
+  #'p*=0.014
+
+#nivel sig = 5 = alpha
+
+#h0 : p=0.01
+
+#h1 : p>0.01
+
+#unilateral Dir
+
+
+#zobs
+#((p* - p) / sqrt(pq / n))
+#((0.014 - 0.01) / sqrt(0.01*(1-0.01) / 1000))  = 1.2713
+
+
+#RC ]z(1-0.05), +inf[
+#]qnorm(1-0.05), +inf[
+#]1.644854, + inf [
+#zobs n€ RC Nao se regeita h0
+
+#ou
+
+#p(z>1.2713 ) = 1-pnorm(1.2713) = 0.101811
+
+#como pvalue > alpha ent nao se rejeita h0
+
+
+#COM CODIG
+
+amostra2  <-c(rep(1,14),rep(0,1000-14))
+
+BSDA::z.test(
+  x=amostra2,
+  sigma.x=sqrt(0.01*(1-0.01)), #sd da amostra como é prop é raiz de pq
+  alternative ="greater",
+  mu=0.01
+)
+
+
+#exemplo 2.2
+
+
+#ho p >=0.01
+
+#h1 p<0.01
+
+#teste esquerdo
+#alpha =0.10
+
+
+
+#((0.014 - 0.01) / sqrt(0.01*(1-0.01) / 1000))  = 1.2713
+
+#rc ]-inf , qnorm(0.10)]
+#rc ]inf , -1.281552]
+
+#como zbos  nao € na RC logo nao se rejeita o h0
+
+
+#valor p : p (z<1.2713) = pnorm(1.2713) =0.898189
+#como valor p >alpha ent nao se rejeita h0
+
+
+#exemplo 2.3
+
+#ho p =0.01
+
+#h1 p=/=0.01
+
+#((0.014 - 0.01) / sqrt(0.01*(1-0.01) / 1000))  = 1.2713
+
+#RC∈]-∞,-z_{α\/2}] ∪[z_{α\/2},+∞[ 
+
+#]-∞,-qnorm(1-(0.05/2))] ∪[qnorm(1-(0.05/2)),+∞[ 
+
+
+
+
+#exemplo 3
+
+#ho = mu1-mu2 =0
+#h1 mu1-me2 |= 0
+
+#teste bi lateral
+
+amostra31<- c(rep(64,9),rep(72,16),rep(74,2),rep(90,5))
+amostra32<- c(rep(70,8),rep(74,22),rep(76,2),rep(90,4))
+
+length(amostra31)
+length(amostra32)
+mean(amostra31)
+mean(amostra32)
+sd(amostra31)
+sd(amostra32)
+
+#zobs = (((x̅1 - x̅2) - (μ1 - μ2)) / sqrt((σ1^2 / n1) + (σ2^2 / n2))) ~ N(0, 1)
+#zobs = ((72.6875 - 75) / sqrt((8.40292^2 / 32) + (5.6669^2 / 36))) =-1.31
+´
+BSDA::z.test(x=amostra31,y=amostra32,sigma.x = sd(amostra31), sigma.y = sd(amostra32),mu=0,alternative = "two.sided")
+
+
